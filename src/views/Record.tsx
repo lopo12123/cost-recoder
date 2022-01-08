8bc7b257ae6a8dd8aa51e74205668bb4a0bdf895
lopo <lopoflyfly@gmail.com>
2022-01-08 18:06:15 +0800

    feat: record page`s layout

--- src/views/Record.tsx
+++ src/views/Record.tsx
@@ -3,13 +3,48 @@ import { StyleSheet, Text, View } from "react-native";
 
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    chart: {
+        width: '70%',
+        height: '60%',
+        backgroundColor: '#cccccc',
+        borderRadius: 5,
+        display: 'flex',  // todo
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    operate: {
+        width: '70%',
+        height: '15%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    inBtn: {
 
+    },
+    outBtn: {
+
+    }
 })
 
 const Record = () => {
     return (
-        <View>
-            <Text>jilu</Text>
+        <View style={styles.container}>
+            <View style={styles.chart}>
+                <Text>Here will show last week`s record</Text>
+            </View>
+            <View style={styles.operate}>
+                <Text>IN</Text>
+                <Text>OUT</Text>
+            </View>
         </View>
     )
 }
