f18d56d651ad31eaaf9963d4231e74560271bc86
lopo <lopoflyfly@gmail.com>
2022-01-09 23:55:19 +0800

    feat: add asyncStorage`s usage

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -10,6 +10,9 @@ import Record from "./src/views/Record";
 import Overview from "./src/views/Overview";
 import Settings from './src/views/Settings';
 
+import { checkStores } from "./src/scripts/store";
+import { ToastAndroid } from "react-native";
+
 const Tab = createBottomTabNavigator();
 
 const RecordIcon = ({ focused }: { focused: boolean }) => { return <AweIcon name="money" size={20} color={focused ? "#4f8ef7" : "#999999"} /> }
@@ -17,6 +20,18 @@ const OverviewIcon = ({ focused }: { focused: boolean }) => { return <AweIcon na
 const SettingIcon = ({ focused }: { focused: boolean }) => { return <AweIcon name="gears" size={20} color={focused ? "#4f8ef7" : "#999999"} /> }
 
 const App = () => {
+    // create the storage if not exist
+    // useEffect(() => {
+    //     checkStores()
+    //         .then((result) => {
+    //             if(!result) ToastAndroid.show('Error initializing database', ToastAndroid.SHORT)
+    //             else ToastAndroid.show('Successfully initialized the database', ToastAndroid.SHORT)
+    //         })
+    //         .catch((e) => {
+    //             ToastAndroid.show('Error initializing database', ToastAndroid.SHORT)
+    //         })
+    // }, [])
+
     return (
         <NavigationContainer>
             <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
