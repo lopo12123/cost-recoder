2ea583c15d68a0cb3be372bbbdaf6324ea149716
lopo <lopoflyfly@gmail.com>
2022-01-09 17:41:32 +0800

    feat: layout in record

--- src/views/Record.tsx
+++ src/views/Record.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { ScrollView, StyleSheet } from "react-native";
+import { Alert, Modal, ScrollView, StyleSheet, Text } from "react-native";
 
 // @ts-ignore
 import AntIcon from 'react-native-vector-icons/AntDesign'
 
-import Card from "../components/Record/Card";
+import Card from "../components/Record/Card"
+import RecordDialog from '../components/Record/RecordDialog'
 
 const styles = StyleSheet.create({
     container: {
@@ -22,12 +23,47 @@ const BankIcon = () => { return <AntIcon name="bank" size={30} color="#e6a23c" /
 const CashIcon = () => { return <AntIcon name="wallet" size={30} color="#f56c6c" /> }
 
 const Record = () => {
+    const [visible, setVisible] = useState(false)
+    const [title, setTitle]: [('Alipay' | 'WeChat' | 'Bank Card' | 'Cash in pocket'), Function] = useState('Alipay')
+    const [inout, setInout]: [('IN' | 'OUT'), Function] = useState('IN')
+
+    const closeModel = () => {
+        setVisible(false)
+    }
+
     return (
         <ScrollView style={styles.container}>
-            <Card title="Alipay" icon={<AlipayIcon/>} />
-            <Card title="WeChat" icon={<WeChatIcon/>} />
-            <Card title="Bank Card" icon={<BankIcon/>} />
-            <Card title="Cash in Pocket" icon={<CashIcon/>} />
+            <Modal visible={visible}
+                   animationType="slide">
+                <RecordDialog title={title} inout={inout} toClose={closeModel} />
+            </Modal>
+            <Card title="Alipay" icon={<AlipayIcon/>}
+                  onClick={(type: 'IN' | 'OUT') => {
+                      setTitle('Alipay')
+                      setInout(type)
+                      setVisible(true)
+                  }} />
+
+            <Card title="WeChat" icon={<WeChatIcon/>}
+                  onClick={(type: 'IN' | 'OUT') => {
+                      setTitle('WeChat')
+                      setInout(type)
+                      setVisible(true)
+                  }} />
+
+            <Card title="Bank Card" icon={<BankIcon/>}
+                  onClick={(type: 'IN' | 'OUT') => {
+                      setTitle('Bank Card')
+                      setInout(type)
+                      setVisible(true)
+                  }} />
+
+            <Card title="Cash in Pocket" icon={<CashIcon/>}
+                  onClick={(type: 'IN' | 'OUT') => {
+                      setTitle('Cash in Pocket')
+                      setInout(type)
+                      setVisible(true)
+                  }} />
         </ScrollView>
     )
 }
