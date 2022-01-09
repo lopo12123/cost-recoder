272a6a6054b57fe498b37cdb3eba13985bf48072
lopo <lopoflyfly@gmail.com>
2022-01-09 22:47:33 +0800

    style: finish the layout

--- src/components/Record/RecordDialog.tsx
+++ src/components/Record/RecordDialog.tsx
@@ -1,45 +1,112 @@
-import React from "react";
-import { Button, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    TextInput, ToastAndroid,
+    TouchableOpacity,
+    View,
+} from "react-native";
 
 const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        padding: 20,
+        padding: 40,
+        backgroundColor: '#efefef',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inner: {
+        width: '100%',
+        padding: 10,
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        elevation: 5,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    title: {
+        marginVertical: 10,
+        fontSize: 30,
+        fontFamily: 'cursive',
+        lineHeight: 36,
     },
     info: {
-        marginVertical: 20,
+        marginVertical: 10,
         fontSize: 24,
         fontFamily: 'cursive',
         lineHeight: 36,
     },
     inputBox: {
+        marginVertical: 10,
         borderColor: '#777777',
         borderBottomWidth: 1,
-        marginVertical: 40,
         fontFamily: 'cursive',
         fontSize: 20,
+    },
+    buttonGroup: {
+        width: '100%',
+        height: 40,
+        marginVertical: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    customButton: {
+        paddingHorizontal: 10,
+        fontSize: 36,
+        fontFamily: 'cursive',
+        lineHeight: 40,
+        textDecorationLine: 'underline'
     }
 })
 
+const CustomButton = (props: { title: string, onClick: Function }) => {
+    return (
+        <TouchableOpacity onPress={() => { props.onClick() }}>
+            <Text style={styles.customButton}>{props.title}</Text>
+        </TouchableOpacity>
+    )
+}
+
 const RecordDialog = (props: { title: 'Alipay' | 'WeChat' | 'Bank Card' | 'Cash in pocket', inout: 'IN' | 'OUT', toClose: Function }) => {
+    const [value, setValue] = useState('')
+
     return (
         <View style={styles.container}>
-            <Text style={styles.info}>
-                There is a saying that if you buy, you earn, the more you buy, the more you earn, so how much do you earn today
-            </Text>
+            <View style={styles.inner}>
+                <Text style={styles.title}>{props.inout === 'IN' ? 'Income' : 'Expenditure'}</Text>
+                <Text style={styles.info}>
+                    There is a saying that if you buy, you earn, the more you buy, the more you earn, so how much do you earn today?
+                </Text>
 
-            <KeyboardAvoidingView behavior="position">
                 <TextInput style={styles.inputBox}
                            keyboardType="numeric"
+                           value={value}
+                           onChangeText={(text) => { setValue(text) }}
                            placeholder={
                                "Enter the amount you " + (props.inout === 'IN' ? 'gain' : 'spent') + " here"
-                           }/>
-            </KeyboardAvoidingView>
+                           } />
 
-            <Button title="cancel" onPress={() => { props.toClose() }}/>
-
-            <Button title="check" onPress={() => { props.toClose() }}/>
+                <View style={styles.buttonGroup}>
+                    <CustomButton title="no" onClick={props.toClose} />
+                    <CustomButton title="yes" onClick={() => {
+                        if(value.trim() === '') {
+                            ToastAndroid.show('Your input is empty!', ToastAndroid.SHORT)
+                        }
+                        else if(parseFloat(value).toString() !== value) {
+                            ToastAndroid.show('your input is not a number!', ToastAndroid.SHORT)
+                        }
+                        else {
+                            ToastAndroid.show('hello', 500)
+                            console.log(props.title, props.inout);
+                            props.toClose()
+                        }
+                    }} />
+                </View>
+            </View>
         </View>
     )
 }
