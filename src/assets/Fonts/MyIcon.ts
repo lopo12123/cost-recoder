fb890c6b0c272901d663e1240626968399b3912e
lopo <lopoflyfly@gmail.com>
2022-01-09 17:41:10 +0800

    fonts: iconfonts

---
+++ src/assets/Fonts/MyIcon.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import createIconSet from "react-native-vector-icons/lib/create-icon-set";
+import glyphMap from './iconfont.json'
+
+const iconSet = createIconSet(glyphMap, "iconfont", "iconfont.ttf")
+
+export default iconSet;
