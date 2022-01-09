f2c0942267f35bb19410eaa2a9519e7582a3be23
lopo <lopoflyfly@gmail.com>
2022-01-09 17:41:21 +0800

    feat: card in record

--- src/components/Record/Card.tsx
+++ src/components/Record/Card.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import MyIcon from "../../assets/Fonts/MyIcon";
 
 const styles = StyleSheet.create({
     container: {
@@ -21,16 +23,44 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         fontSize: 18,
         fontFamily: 'cursive',
+    },
+    moneyIn: {
+        position: 'absolute',
+        width: 30,
+        height: 30,
+        right: 70,
+    },
+    moneyOut: {
+        position: 'absolute',
+        width: 30,
+        height: 30,
+        right: 20,
     }
 })
 
-const Card = (props: { title: string, icon?: JSX.Element, onClickIn?: Function, onClickOut?: Function }) => {
+const MoneyOut = () => { return <MyIcon name="money-out" size={30} color="green" /> }
+const MoneyIn = () => { return <MyIcon name="money-in" size={30} color="red" /> }
+
+const Card = (props: { title: string, icon?: JSX.Element, onClick?: Function }) => {
     return (
         <View style={styles.container}>
             {props.icon}
-            <Text style={styles.text}>
-                {props.title}
-            </Text>
+            <Text style={styles.text}>{props.title}</Text>
+
+            <TouchableOpacity style={styles.moneyIn}
+                              onPress={() => {
+                                  props.onClick ? props.onClick('IN') : null
+                              }}>
+                <MoneyIn />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.moneyOut}
+                              onPress={() => {
+                                  props.onClick ? props.onClick('OUT') : null
+                              }}>
+                <MoneyOut />
+            </TouchableOpacity>
+
         </View>
     )
 }
