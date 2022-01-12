0260350249589782ea09e45cf0378b49da60de47
lopo <lopoflyfly@gmail.com>
2022-01-12 22:28:31 +0800

    style: change the card`s style

--- src/components/Overview/OverviewHome.tsx
+++ src/components/Overview/OverviewHome.tsx
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 // @ts-ignore
 import AweIcon from 'react-native-vector-icons/FontAwesome'
 
+import CalendarView from "./CalendarView";
 import Card from "./Card";
 
 const styles = StyleSheet.create({
@@ -17,29 +18,47 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-start'
     },
+    calendar: {
+        width: '100%',
+        flex: 1,
+    },
+    buttonGroup: {
+        width: '100%',
+        height: 50,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    }
 })
 
-const BarIcon = () => { return <AweIcon name="bar-chart" size={30} color="#f56c6c" /> }
-const LineIcon = () => { return <AweIcon name="line-chart" size={30} color="#67c23a" /> }
-const PieIcon = () => { return <AweIcon name="pie-chart" size={30} color="#e6a23c" /> }
+const BarIcon = () => { return <AweIcon name="bar-chart" size={25} color="#f56c6c" /> }
+const LineIcon = () => { return <AweIcon name="line-chart" size={25} color="#67c23a" /> }
+const PieIcon = () => { return <AweIcon name="pie-chart" size={25} color="#e6a23c" /> }
 
 const OverviewHome = ({navigation}: any) => {
     return (
         <View style={styles.container}>
-            <Card title="Bar Overview" detail="The specific number of each account"
-                  icon={<BarIcon/>} onClick={() => {
-                      navigation.navigate('BarOverview')
-            }} />
+            <View style={styles.calendar}>
+                <CalendarView />
+            </View>
 
-            <Card title="Line Overview" detail="Changes in each account"
-                  icon={<LineIcon/>} onClick={() => {
-                navigation.navigate('LineOverview')
-            }} />
+            <View style={styles.buttonGroup}>
+                <Card title="Bar View" detail="The specific number of each account"
+                      icon={<BarIcon/>} onClick={() => {
+                    navigation.navigate('BarOverview')
+                }} />
 
-            <Card title="Pie Overview" detail="The proportion of each account"
-                  icon={<PieIcon/>} onClick={() => {
-                navigation.navigate('PieOverview')
-            }} />
+                <Card title="Line View" detail="Changes in each account"
+                      icon={<LineIcon/>} onClick={() => {
+                    navigation.navigate('LineOverview')
+                }} />
+
+                <Card title="Pie View" detail="The proportion of each account"
+                      icon={<PieIcon/>} onClick={() => {
+                    navigation.navigate('PieOverview')
+                }} />
+            </View>
         </View>
     )
 }
