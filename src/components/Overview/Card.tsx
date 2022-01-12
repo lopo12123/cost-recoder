4c694a4ca4a258d219fb725b94dd9cf5217dd142
lopo <lopoflyfly@gmail.com>
2022-01-12 22:28:27 +0800

    style: change the card`s style

--- src/components/Overview/Card.tsx
+++ src/components/Overview/Card.tsx
@@ -5,44 +5,28 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 const styles = StyleSheet.create({
     container: {
         position: 'relative',
-        width: '100%',
-        height: 60,
-        marginVertical: 5,
-        marginHorizontal: 0,
-        paddingHorizontal: 20,
+        width: 100,
+        height: 50,
         borderRadius: 10,
         backgroundColor: '#ffffff',
         elevation: 5,
         display: 'flex',
-        flexDirection: 'row',
+        // flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'flex-start',
+        justifyContent: 'center'
     },
     title: {
-        marginLeft: 10,
-        fontSize: 20,
-        fontWeight: 'bold',
-        fontFamily: 'cursive',
-    },
-    detail: {
-        marginLeft: 10,
         fontSize: 14,
-        fontFamily: 'cursive',
-    }
+        fontWeight: 'bold',
+        fontFamily: 'cursive'
+    },
 })
 
 const Card = (props: { title: string, detail?: string, icon?: JSX.Element, onClick?: Function }) => {
     return (
         <TouchableOpacity style={styles.container} onPress={() => { props.onClick ? props.onClick() : null }} >
             {props.icon}
-            <View>
-                <Text style={styles.title}>
-                    {props.title}
-                </Text>
-                <Text style={styles.detail}>
-                    {props.detail}
-                </Text>
-            </View>
+            <Text style={styles.title}>{props.title}</Text>
         </TouchableOpacity>
     )
 }
